4ff4cdeb5b96517ed574afe8ff8a0afc0277f291
JohnWilliams1990 <JohnWilliams199032@gmail.com>
2018-06-06 23:43:28 -0600

    change using chef-client command

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+if node[:os].downcase == 'linux' then 
+
+puts "We are on a linux system"
+
+end
+
+puts  "Name: "+ node[:name]
+puts  "Operating system: " + node[:os] 
+puts "System: " + node['virtualization']['system']
+puts "Ip Address: " + node['ipaddress']
+
---
+++ recipes/info.rb
@@ -0,0 +1,31 @@
+
+
+
+
+log "Ip Address:  #{node['ipaddress']}"
+log "MAC Address:  #{node['macaddress']}"
+log "OS Platform:  #{node['platform']}  #{node['platform_version']}"
+log "Running on a :  #{node['virtualization']['system']} #{node['virtualization']['role']}"
+log "HostName:  #{node['hostname']}"
+
+
+
+#puts  "Ip Address:  #{node['ipaddress']}"
+#puts "MAC Address:  #{node['macaddress']}"
+#puts "OS Platform:  #{node['platform']}  #{node['platform_version']}"
+#puts "Running on a :  #{node['virtualization']['system']} #{node['virtualization']['role']}"
+#puts "HostName:  #{node['hostname']}"
+
+
+
+
+
+
+#puts  "Name: "+ node[:name]
+#puts  "Operating system: " + node[:os]
+#puts "System: " + node['virtualization']['system']
+#puts "Ip Address: " + node['ipaddress']
+
+
+
+
